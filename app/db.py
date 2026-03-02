bf4940f812039f307422920ac29feb77e0376ae5
Michael Kidane Melles <139519727+michael2747-j@users.noreply.github.com>
2026-03-02 10:31:50 -0500

    Add SSL mode for Render

--- app/db.py
+++ app/db.py
@@ -22,7 +22,7 @@ DB_CONFIG = {
 
 def get_connection_string():
     """Generate PostgreSQL connection string"""
-    return f"host={DB_CONFIG['host']} port={DB_CONFIG['port']} dbname={DB_CONFIG['dbname']} user={DB_CONFIG['user']} password={DB_CONFIG['password']}"
+    return f"host={DB_CONFIG['host']} port={DB_CONFIG['port']} dbname={DB_CONFIG['dbname']} user={DB_CONFIG['user']} password={DB_CONFIG['password']} sslmode=require"
 
 @contextmanager
 def get_db_connection():
